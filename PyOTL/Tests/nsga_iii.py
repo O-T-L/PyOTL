7a788961799abc61a519f13cd3d95b6d48901d20
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2014-12-19 22:03:29 +0800

    update

--- PyOTL/Tests/nsga_iii.py
+++ PyOTL/Tests/nsga_iii.py
@@ -36,7 +36,7 @@ class TestCase(unittest.TestCase):
 	def tearDown(self):
 		pass
 	
-	def testNSGA_III_NBI(self):
+	def testNSGA_III_NBI_DTLZ2(self):
 		random = pyotl.utility.Random(1)
 		problemGen = lambda: pyotl.problem.real.DTLZ2(3)
 		problem = problemGen()
@@ -71,7 +71,44 @@ class TestCase(unittest.TestCase):
 		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
 		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
 	
-	def testNSGA_III_NBI2(self):
+	def testNSGA_III_NBI_NegativeDTLZ2(self):
+		random = pyotl.utility.Random(1)
+		problemGen = lambda: pyotl.problem.real.NegativeDTLZ2(3)
+		problem = problemGen()
+		pathProblem = os.path.join(self.pathData, type(problem).__name__.replace('Negative', ''), str(problem.GetNumberOfObjectives()))
+		_crossover = pyotl.crossover.real.SimulatedBinaryCrossover(random, 1, problem.GetBoundary(), 20)
+		crossover = pyotl.crossover.real.CoupleCoupleCrossoverAdapter(_crossover, random)
+		mutation = pyotl.mutation.real.PolynomialMutation(random, 1 / float(len(problem.GetBoundary())), problem.GetBoundary(), 20)
+		_referenceSet = pyotl.utility.NormalBoundaryIntersection_Real(problem.GetNumberOfObjectives(), 12)
+		size = len(_referenceSet)
+		while (size % 4):
+			size += 1
+		pfList = []
+		for _ in range(self.repeat):
+			problem = problemGen()
+			initial = pyotl.initial.real.PopulationUniform(random, problem.GetBoundary(), size)
+			optimizer = pyotl.optimizer.real.NSGA_III(random, problem, initial, crossover, mutation, _referenceSet)
+			for _ in range(300):
+				optimizer()
+			pf = pyotl.utility.PyListList2VectorVector_Real([list(solution.objective_) for solution in optimizer.GetSolutionSet()])
+			for objective in pf:
+				problem.Fix(objective)
+			pfList.append(pf)
+		pathCrossover = os.path.join(pathProblem, type(crossover.GetCrossover()).__name__)
+		pathOptimizer = os.path.join(pathCrossover, type(optimizer).__name__)
+		pfTrue = pyotl.utility.PyListList2VectorVector_Real(numpy.loadtxt(os.path.join(pathProblem, 'PF.csv')).tolist())
+		#GD
+		indicator = pyotl.indicator.real.DTLZ2GD()
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'GD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+		#IGD
+		indicator = pyotl.indicator.real.InvertedGenerationalDistance(pfTrue)
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+	
+	def testNSGA_III_NBI2_DTLZ2(self):
 		random = pyotl.utility.Random(1)
 		problemGen = lambda: pyotl.problem.real.DTLZ2(8)
 		problem = problemGen()
