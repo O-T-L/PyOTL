d0abd36346e065dba7c881167943eb31cdb3b2ec
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2014-12-19 21:18:15 +0800

    udpate for Negative DTLZ2

--- PyOTL/Tests/moea_d.py
+++ PyOTL/Tests/moea_d.py
@@ -37,7 +37,7 @@ class TestCase(unittest.TestCase):
 	def tearDown(self):
 		pass
 	
-	def testMOEA_D_WeightedSum(self):
+	def testMOEA_D_WeightedSum_DTLZ2(self):
 		random = pyotl.utility.Random(1)
 		problemGen = lambda: pyotl.problem.real.DTLZ2(3)
 		problem = problemGen()
@@ -69,7 +69,41 @@ class TestCase(unittest.TestCase):
 		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
 		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
 	
-	def testMOEA_D_Tchebycheff(self):
+	def testMOEA_D_WeightedSum_NegativeDTLZ2(self):
+		random = pyotl.utility.Random(1)
+		problemGen = lambda: pyotl.problem.real.NegativeDTLZ2(3)
+		problem = problemGen()
+		pathProblem = os.path.join(self.pathData, type(problem).__name__.replace('Negative', ''), str(problem.GetNumberOfObjectives()))
+		crossover = pyotl.crossover.real.SimulatedBinaryCrossover(random, 1, problem.GetBoundary(), 20)
+		mutation = pyotl.mutation.real.PolynomialMutation(random, 1 / float(len(problem.GetBoundary())), problem.GetBoundary(), 20)
+		weightVectors = pyotl.utility.NormalBoundaryIntersection_Real(problem.GetNumberOfObjectives(), 23)
+		neighborRatio = 0.1
+		pfList = []
+		for _ in range(self.repeat):
+			problem = problemGen()
+			initial = pyotl.initial.real.PopulationUniform(random, problem.GetBoundary(), len(weightVectors))
+			optimizer = pyotl.optimizer.couple_couple.real.MOEA_D_WeightedSum(random, problem, initial, crossover, mutation, weightVectors, int(len(weightVectors) * neighborRatio))
+			while optimizer.GetProblem().GetNumberOfEvaluations() < 30000:
+				optimizer()
+			pf = pyotl.utility.PyListList2VectorVector_Real([list(solution.objective_) for solution in optimizer.GetSolutionSet()])
+			for objective in pf:
+				problem.Fix(objective)
+			pfList.append(pf)
+		pathCrossover = os.path.join(pathProblem, type(crossover).__name__)
+		pathOptimizer = os.path.join(pathCrossover, type(optimizer).__name__)
+		pfTrue = pyotl.utility.PyListList2VectorVector_Real(numpy.loadtxt(os.path.join(pathProblem, 'PF.csv')).tolist())
+		#GD
+		indicator = pyotl.indicator.real.DTLZ2GD()
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'GD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+		#IGD
+		indicator = pyotl.indicator.real.InvertedGenerationalDistance(pfTrue)
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+	
+	def testMOEA_D_Tchebycheff_DTLZ2(self):
 		random = pyotl.utility.Random(1)
 		problemGen = lambda: pyotl.problem.real.DTLZ2(3)
 		problem = problemGen()
@@ -103,7 +137,43 @@ class TestCase(unittest.TestCase):
 		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
 		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
 	
-	def testMOEA_D_PBI(self):
+	def testMOEA_D_Tchebycheff_NegativeDTLZ2(self):
+		random = pyotl.utility.Random(1)
+		problemGen = lambda: pyotl.problem.real.NegativeDTLZ2(3)
+		problem = problemGen()
+		pathProblem = os.path.join(self.pathData, type(problem).__name__.replace('Negative', ''), str(problem.GetNumberOfObjectives()))
+		crossover = pyotl.crossover.real.SimulatedBinaryCrossover(random, 1, problem.GetBoundary(), 20)
+		mutation = pyotl.mutation.real.PolynomialMutation(random, 1 / float(len(problem.GetBoundary())), problem.GetBoundary(), 20)
+		weightVectors = pyotl.utility.NormalBoundaryIntersection_Real(problem.GetNumberOfObjectives(), 23)
+		for weight in weightVectors:
+			pyotl.optimizer.moea_d.AdjustWeight_Real(weight, 0.00001)
+		neighborRatio = 0.1
+		pfList = []
+		for _ in range(self.repeat):
+			problem = problemGen()
+			initial = pyotl.initial.real.PopulationUniform(random, problem.GetBoundary(), len(weightVectors))
+			optimizer = pyotl.optimizer.couple_couple.real.MOEA_D_Tchebycheff(random, problem, initial, crossover, mutation, weightVectors, int(len(weightVectors) * neighborRatio))
+			while optimizer.GetProblem().GetNumberOfEvaluations() < 30000:
+				optimizer()
+			pf = pyotl.utility.PyListList2VectorVector_Real([list(solution.objective_) for solution in optimizer.GetSolutionSet()])
+			for objective in pf:
+				problem.Fix(objective)
+			pfList.append(pf)
+		pathCrossover = os.path.join(pathProblem, type(crossover).__name__)
+		pathOptimizer = os.path.join(pathCrossover, type(optimizer).__name__)
+		pfTrue = pyotl.utility.PyListList2VectorVector_Real(numpy.loadtxt(os.path.join(pathProblem, 'PF.csv')).tolist())
+		#GD
+		indicator = pyotl.indicator.real.DTLZ2GD()
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'GD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+		#IGD
+		indicator = pyotl.indicator.real.InvertedGenerationalDistance(pfTrue)
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+	
+	def testMOEA_D_PBI_DTLZ2(self):
 		random = pyotl.utility.Random(1)
 		problemGen = lambda: pyotl.problem.real.DTLZ2(3)
 		problem = problemGen()
@@ -123,6 +193,43 @@ class TestCase(unittest.TestCase):
 			while optimizer.GetProblem().GetNumberOfEvaluations() < 30000:
 				optimizer()
 			pf = pyotl.utility.PyListList2VectorVector_Real([list(solution.objective_) for solution in optimizer.GetSolutionSet()])
+			pfList.append(pf)
+		pathCrossover = os.path.join(pathProblem, type(crossover).__name__)
+		pathOptimizer = os.path.join(pathCrossover, type(optimizer).__name__)
+		pfTrue = pyotl.utility.PyListList2VectorVector_Real(numpy.loadtxt(os.path.join(pathProblem, 'PF.csv')).tolist())
+		#GD
+		indicator = pyotl.indicator.real.DTLZ2GD()
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'GD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+		#IGD
+		indicator = pyotl.indicator.real.InvertedGenerationalDistance(pfTrue)
+		metricList = [indicator(pf) for pf in pfList]
+		rightList = numpy.loadtxt(os.path.join(pathOptimizer, 'IGD.csv')).tolist()
+		self.assertGreater(scipy.stats.ttest_ind(rightList, metricList)[1], 0.05, [numpy.mean(rightList), numpy.mean(metricList), metricList])
+	
+	def testMOEA_D_PBI_NegativeDTLZ2(self):
+		random = pyotl.utility.Random(1)
+		problemGen = lambda: pyotl.problem.real.NegativeDTLZ2(3)
+		problem = problemGen()
+		pathProblem = os.path.join(self.pathData, type(problem).__name__.replace('Negative', ''), str(problem.GetNumberOfObjectives()))
+		crossover = pyotl.crossover.real.SimulatedBinaryCrossover(random, 1, problem.GetBoundary(), 20)
+		mutation = pyotl.mutation.real.PolynomialMutation(random, 1 / float(len(problem.GetBoundary())), problem.GetBoundary(), 20)
+		weightVectors = pyotl.utility.NormalBoundaryIntersection_Real(problem.GetNumberOfObjectives(), 23)
+		for weight in weightVectors:
+			pyotl.optimizer.moea_d.NormalizeWeight_Real(weight)
+		neighborRatio = 0.1
+		penaltyParameter = 5
+		pfList = []
+		for _ in range(self.repeat):
+			problem = problemGen()
+			initial = pyotl.initial.real.PopulationUniform(random, problem.GetBoundary(), len(weightVectors))
+			optimizer = pyotl.optimizer.couple_couple.real.MOEA_D_PBI(random, problem, initial, crossover, mutation, weightVectors, int(len(weightVectors) * neighborRatio), penaltyParameter)
+			while optimizer.GetProblem().GetNumberOfEvaluations() < 30000:
+				optimizer()
+			pf = pyotl.utility.PyListList2VectorVector_Real([list(solution.objective_) for solution in optimizer.GetSolutionSet()])
+			for objective in pf:
+				problem.Fix(objective)
 			pfList.append(pf)
 		pathCrossover = os.path.join(pathProblem, type(crossover).__name__)
 		pathOptimizer = os.path.join(pathCrossover, type(optimizer).__name__)
