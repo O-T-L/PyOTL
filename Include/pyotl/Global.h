f0f195e6e95043bcd71e9d9bd91ba2f0baa4c55f
Ruimin Shen <ruimin0shen@gmail.com>
2015-10-12 09:57:38 +0800

    update

--- Include/pyotl/Global.h
+++ Include/pyotl/Global.h
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include <list>
 #include <random>
-#include <boost/python/overloads.hpp>
 #include <boost/dynamic_bitset.hpp>
 
 namespace pyotl
